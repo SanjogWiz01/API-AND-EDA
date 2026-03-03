5414cc8790f09838ad288935778599d4148638e6
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-03-03 10:22:44 +0545

    f

--- Categorial Encoding/one_hot_encoding.py
+++ Categorial Encoding/one_hot_encoding.py
@@ -13,3 +13,8 @@ print(encoded)
 df_encoded = pd.get_dummies(df, columns=["City"])
 
 print(df_encoded)
+''' Pokhara = [0 0 1]
+Kathmandu = [0 1 0]
+Butwal = [1 0 0]
+advantage = simple
+disadvanatges = create many columns'''
